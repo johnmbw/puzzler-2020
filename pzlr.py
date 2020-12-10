197b261c8aa134252737581eae0810a7d50f1ed3
John Montgomery <johnm@brandwatch.com>
2020-12-10 08:29:02 +0000

    even out line lengths + fix check for word length

--- pzlr.py
+++ pzlr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-G=['brloty','rceolb','chgoer','hswogc','synowh','ybtons','lbreot','ercgol','gechwo','woghsn','ntowsy','tblony','oblrecghwsnyt']
-E,L={p for vs in G for p in [vs[0] + vs[i] for i in range(1, len(vs))]}, set(''.join(G))
-ws = {l.strip().lower() for l in open('/usr/share/dict/words')}
-[print(w) for w in sorted(ws) if w in L or (len(w) > 0 and all(w[i:i+2] in E for i in range(len(w)-1)))]
+G='brloty rceolb chgoer hswogc synowh ybtons lbreot ercgol gechwo woghsn ntowsy tblony oblrecghwsnyt'
+E,L={p for vs in G.split() for p in [vs[0] + vs[i] for i in range(1, len(vs))]}, set(''.join(G))
+ws = sorted({l.strip().lower() for l in open('/usr/share/dict/words')})
+[print(w) for w in ws if w in L or (len(w) > 1 and all(w[i:i+2] in E for i in range(len(w)-1)))]
