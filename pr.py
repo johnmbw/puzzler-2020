5bed4813fb2714f08e6fde28834212cb6698ab68
John Montgomery <johnm@brandwatch.com>
2020-12-16 09:50:21 +0000

    tiny bit more space saving

--- pr.py
+++ pr.py
@@ -1,3 +1,3 @@
-G='brloty rceol chgoe hswog synow yton leot ego gwo won nto to'
-E=set(G);[[E.update((v[0]+i,i+v[0]))for i in v[1:]]for v in G.split()]
-[print(w)for w in{l.strip().lower()for l in open('/usr/share/dict/words')}if {w[i:i+2]for i in range(len(w))}<=E]
+G='brlty rcel chge hswg synw ytn let eg gw wn nt t'
+E=set(G+'o');[[E.update((v[0]+i,i+v[0]))for i in v[1:]+'o']for v in G.split()]
+[print(w)for w in{l[:-1].lower()for l in open('/usr/share/dict/words')}if{w[i:i+2]for i in range(len(w))}<=E]
