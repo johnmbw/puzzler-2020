dccad6b293142d45ef70dfbf08370e4c05256ac5
John Montgomery <johnm@brandwatch.com>
2020-12-16 09:55:54 +0000

    read filename from command line
    which also shaves off one char

--- pr.py
+++ pr.py
@@ -1,3 +1,3 @@
-G='brlty rcel chge hswg synw ytn let eg gw wn nt t'
+G='brlty rcel chge hswg synw ytn let eg gw wn nt t';import sys
 E=set(G+'o');[[E.update((v[0]+i,i+v[0]))for i in v[1:]+'o']for v in G.split()]
-[print(w)for w in{l[:-1].lower()for l in open('/usr/share/dict/words')}if{w[i:i+2]for i in range(len(w))}<=E]
+[print(w)for w in{l[:-1].lower()for l in open(sys.argv[1])}if{w[i:i+2]for i in range(len(w))}<=E]
