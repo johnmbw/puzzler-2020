089bc46d2f5922b03579071d0f8b944e82f6ce01
John Montgomery <johnm@brandwatch.com>
2020-12-17 16:30:21 +0000

    bash implementation

---
+++ puzzler.sh
@@ -0,0 +1,1 @@
+tr [A-Z] [a-z]|sort -u|grep -E ^[bceghlnorstwy]+$|grep -Ev 'b[^rloty]|r[^ceolb]|c[^hgoer]|h[^swogc]|s[^ynowh]|y[^btons]|l[^breot]|e[^rcgol]|g[^echwo]|w[^oghsn]|n[^towsy]|t[^blony]|o[^blrecghwsnyt]'
