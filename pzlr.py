67ba38de5860cc02e889ce5a4526af765fe99d59
John Montgomery <johnm@brandwatch.com>
2020-12-18 14:04:58 +0000

    remove 4 line python version

--- pzlr.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-G='brloty rceolb chgoer hswogc synowh ybtons lbreot ercgol gechwo woghsn ntowsy tblony oblrecghwsnyt'
-E,L={p for vs in G.split() for p in [vs[0] + vs[i] for i in range(1, len(vs))]}, set(''.join(G))
-ws = sorted({l.strip() for l in open('/usr/share/dict/words')})
-[print(w) for w in ws if w.lower() in L or (len(w) > 1 and all(w[i:i+2].lower() in E for i in range(len(w)-1)))]
