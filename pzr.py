997e36023754209c733ddc67376f7ad3229abacb
John Montgomery <johnm@brandwatch.com>
2020-12-18 13:56:27 +0000

    keep just one 3 line python version

--- pzr.py
+++ pzr.py
@@ -1,3 +1,3 @@
-G,f='brloty rceol chgoe hswog synow yton leot ego gwo won nto to',lambda l:{i for s in l for i in s}
-E,d=f(f((v[0]+i,i+v[0])for i in v[1:])for v in G.split())|f(G),open('/usr/share/dict/words')
-[print(w)for w in{l.strip().lower()for l in d}if {w[i:i+2]for i in range(len(w))}<=E]
+G='brlty rcel chge hswg synw ytn let eg gw wn nt t';import sys
+E=set(G+'o');[[E.update((v[0]+i,i+v[0]))for i in v[1:]+'o']for v in G.split()]
+[print(w)for w in{l[:-1]for l in open(sys.argv[1])}if{w[i:i+2].lower()for i in range(len(w))}<=E]
